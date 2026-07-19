5f324368283d0b7bdd50364742c23a3e23d6b823
barasapeter <barasapeter52@gmail.com>
2026-07-19 23:32:01 +0300

    Refined worker_detail.html

--- fastapi_endpoints_with_ws.py
+++ fastapi_endpoints_with_ws.py
@@ -452,4 +452,4 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")
